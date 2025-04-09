2ce1575d99b7647401d655bb90535f16389595bc
andrew-fisher1 <113075629+andrew-fisher1@users.noreply.github.com>
2025-04-09 15:22:34 -0400

    most recent, added sprites

--- node_3d.gd
+++ node_3d.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
-var bird_model_root: Node3D = null
+var model_root: Node3D = null
+var model_resource = null
+var model = null
+
 var animation_player: AnimationPlayer = null
 var current_tween: Tween = null
 
@@ -41,43 +44,68 @@ func on_tween_completed() -> void:
 		animation_player.play("anim")
 
 
-func _process(delta: float) -> void:
-	if bird_model_root:
+func _process(_delta: float) -> void:
+	if model_root:
 		current_angle = rad_to_deg(lerp_angle(deg_to_rad(start_angle), deg_to_rad(target_angle), tween_progress))
 		
 		var radians = deg_to_rad(current_angle)
 		var new_position = circle_center + Vector3(cos(radians) * circle_radius, 0, sin(radians) * circle_radius)
-		bird_model_root.global_transform.origin = new_position
+		model_root.global_transform.origin = new_position
 		
 		# Compute radial vector from the bird's position toward the center
 		var radial = (circle_center - new_position).normalized()
 		
-		if tween_progress < 1.0:
-			# Determine the direction of movement from the tween
-			var delta_angle = ((int(target_angle) - int(start_angle) + 180) % 360) - 180
-			var tangent: Vector3
-			
-			if delta_angle > 0:
-				# Rotate 90 clockwise
-				tangent = Vector3(radial.z, 0, -radial.x)
-			else:
-				# Rotate 90 counterclockwise
-				tangent = Vector3(-radial.z, 0, radial.x)
-			tangent = tangent.normalized()
-			
-			var desired_dir = (radial + tangent).normalized()
-			bird_model_root.look_at(new_position - desired_dir, Vector3.UP)
-		else:
-			# Bird is not moving- face center
-			bird_model_root.look_at(new_position - radial, Vector3.UP)
+		#if tween_progress < 1.0:
+			## Determine the direction of movement from the tween
+			#var delta_angle = ((int(target_angle) - int(start_angle) + 180) % 360) - 180
+			#var tangent: Vector3
+			#
+			#if delta_angle > 0:
+				## Rotate 90 clockwise
+				#tangent = Vector3(radial.z, 0, -radial.x)
+			#else:
+				## Rotate 90 counterclockwise
+				#tangent = Vector3(-radial.z, 0, radial.x)
+			#tangent = tangent.normalized()
+			#
+			#var desired_dir = (radial + tangent).normalized()
+			#model_root.look_at(new_position - desired_dir, Vector3.UP)
+		
+		
+		model_root.look_at(new_position - radial, Vector3.UP)
+		
+		# To do on axis rotation
+		if (model == 'mega'):
+			model_root.rotate_y(deg_to_rad(90))  
+			position += Vector3(0, 1, 0)
+
+
 
 func _ready():
-	var model_resource = load("res://bird_orange.glb") as PackedScene
-	if model_resource:
-		bird_model_root = model_resource.instantiate()
-		add_child(bird_model_root)
+	var args = OS.get_cmdline_args()
+	if OS.has_feature("editor"):
+		args = ["--model=mega"] 
 		
-		animation_player = bird_model_root.get_node("AnimationPlayer")
+	for arg in args:
+		if arg.begins_with("--model="):
+			model = arg.split("=")[1]
+			print(model)
+			if (model == "bird"):
+				model_resource = load("res://bird_orange.glb") as PackedScene
+			elif (model == "train"):
+				scale = Vector3(.007, .007, .007)
+				model_resource = load("res://steam_train.glb") as PackedScene
+			elif (model == "mega"):
+				scale = Vector3(.2, .2, .2)
+				model_resource = load("res://megaphone.glb") as PackedScene
+			else:
+				assert(false, "Need to pick a model")
+
+	if model_resource:
+		model_root = model_resource.instantiate()
+		add_child(model_root)
+		
+		animation_player = model_root.get_node("AnimationPlayer")
 		if animation_player:
 			animation_player.play("anim")
 		
@@ -86,4 +114,4 @@ func _ready():
 		target_angle = current_angle
 		tween_progress = 1.0
 	
-		bird_model_root.global_transform.origin = circle_center + Vector3(circle_radius, 0, 0)
+		model_root.global_transform.origin = circle_center + Vector3(circle_radius, 0, 0)
--- speakers.gd
+++ speakers.gd
@@ -4,48 +4,48 @@ var speakers = []
 var number_of_speakers = 8
 var radius_of_speakers = 5.0  # Radius in 3D space
 
-func _ready():
-	for i in range(number_of_speakers):
-		var speaker_mesh = MeshInstance3D.new()
-		speaker_mesh.mesh = BoxMesh.new()  # Replace with your speaker mesh
-		add_child(speaker_mesh)
-		
-		# Calculate position in a circle
-		var angle = deg_to_rad(i * (360.0 / number_of_speakers))
-		var x = cos(angle) * radius_of_speakers
-		var z = sin(angle) * radius_of_speakers
-		speaker_mesh.position = Vector3(x, 0, z)
-		speakers.append(speaker_mesh)
+#func _ready():
+	#for i in range(number_of_speakers):
+		#var speaker_mesh = MeshInstance3D.new()
+		#speaker_mesh.mesh = BoxMesh.new()  # Replace with your speaker mesh
+		#add_child(speaker_mesh)
+		#
+		## Calculate position in a circle
+		#var angle = deg_to_rad(i * (360.0 / number_of_speakers))
+		#var x = cos(angle) * radius_of_speakers
+		#var z = sin(angle) * radius_of_speakers
+		#speaker_mesh.position = Vector3(x, 0, z)
+		#speakers.append(speaker_mesh)
 
 # Replace with this to customize speaker locations instead of hardcoded circle
-#var speaker_positions = [
-#    Vector2(5, 0),  #right mid
-#    Vector2(4.8, 3), #right bot
-#    Vector2(1.5, 4.75), #bottom right
-#    Vector2(-1.5, 4.75), #bottom right
-#    Vector2(-4.8, 3), #left bot
-#    Vector2(-5, 0), #left mid
-#    Vector2(-4.8, -3), #left top
-#    Vector2(-1.5, -4.75), #top left
-#    Vector2(1.5, -4.75), #top right
-#    Vector2(4.8, -3) #right top
-#]
-#var speaker_scene = preload("res://speaker_fnaf.glb")
-#
-#
-#func _ready():
-#    for pos in speaker_positions:
-#        var speaker_instance = speaker_scene.instantiate()
-#        speaker_instance.scale = Vector3(0.004, 0.004, 0.004)
-#
-#        var angle = atan2(pos.y, pos.x)
-#        var adjusted_angle = PI / 2 - angle + deg_to_rad(180)
-#        speaker_instance.rotate_y(adjusted_angle)
-#        #print("Position: ", pos, " | Angle (radians): ", angle, " | Angle (degrees): ", rad_to_deg(angle))
-#        #speaker_instance.rotate_y(deg_to_rad(275+rad_to_deg(angle)))
-#
-#        add_child(speaker_instance)
-#
-#        # Convert (x, y) to 3D coordinates (assuming y corresponds to the Z-axis)
-#        speaker_instance.position = Vector3(pos.x, 0, pos.y)
-#        speakers.append(speaker_instance)
+var speaker_positions = [
+	Vector2(5, 0),  #right mid
+	Vector2(4.8, 3), #right bot
+	Vector2(1.5, 4.75), #bottom right
+	Vector2(-1.5, 4.75), #bottom right
+	Vector2(-4.8, 3), #left bot
+	Vector2(-5, 0), #left mid
+	Vector2(-4.8, -3), #left top
+	Vector2(-1.5, -4.75), #top left
+	Vector2(1.5, -4.75), #top right
+	Vector2(4.8, -3) #right top
+]
+var speaker_scene = preload("res://speaker_fnaf.glb")
+
+
+func _ready():
+	for pos in speaker_positions:
+		var speaker_instance = speaker_scene.instantiate()
+		speaker_instance.scale = Vector3(0.004, 0.004, 0.004)
+
+		var angle = atan2(pos.y, pos.x)
+		var adjusted_angle = PI / 2 - angle + deg_to_rad(180)
+		speaker_instance.rotate_y(adjusted_angle)
+		#print("Position: ", pos, " | Angle (radians): ", angle, " | Angle (degrees): ", rad_to_deg(angle))
+		#speaker_instance.rotate_y(deg_to_rad(275+rad_to_deg(angle)))
+
+		add_child(speaker_instance)
+
+		# Convert (x, y) to 3D coordinates (assuming y corresponds to the Z-axis)
+		speaker_instance.position = Vector3(pos.x, 1, pos.y)
+		speakers.append(speaker_instance)
